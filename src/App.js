098d8245e0b243d79d2697ea5e8369d76b205a80
trevormchaffie <84483437+trevormchaffie@users.noreply.github.com>
2024-05-16 20:19:19 -0400

    ImageLinkForm, Logo, Naviagtion, Rank

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import ParticlesBg from "particles-bg";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesBg type="circle" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
